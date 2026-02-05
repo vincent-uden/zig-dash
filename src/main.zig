12090b8f9a39714f983c6f434c993da07b68c89b
Vincent Udén <vincentuden@gmail.com>
2026-02-05 16:49:42 +0100

    Spawn subprocess journalctl

--- src/main.zig
+++ src/main.zig
@@ -4,21 +4,72 @@ const rl = @import("raylib");
 
 const zeit = @import("zeit");
 
-const JournalJobResult = struct {
-    timestamp: []u8,
-    success: bool,
+const JournalEntry = struct {
+    buffer: []u8,
+    timestamp: zeit.Time,
+    hostname: []u8,
+    unit: []u8,
+    message: []u8,
+
+    fn from_str(buffer: []u8) error{ParsingError}!JournalEntry {
+        var iterator = std.mem.splitScalar(u8, buffer, ' ');
+        const iso_timestamp = try (iterator.next() orelse error.ParsingError);
+        const hostname = try (iterator.next() orelse error.ParsingError);
+        const unit = try (iterator.next() orelse error.ParsingError);
+
+        const timestamp = zeit.Time.fromISO8601(iso_timestamp);
+
+        const out: JournalEntry = .{
+            .buffer = buffer,
+            .hostname = hostname,
+            .unit = unit,
+            .timestamp = timestamp,
+        };
+
+        return out;
+    }
 };
 
 pub fn main() anyerror!void {
-    const allocator = std.heap.page_allocator;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
     var env = try std.process.getEnvMap(allocator);
     defer env.deinit();
 
     const now = try zeit.instant(.{});
-    std.debug.print("{}", .{now});
+    std.debug.print("{}\n", .{now});
 
     const screenWidth = 800;
     const screenHeight = 450;
+
+    // Call journalctl
+    var child = std.process.Child.init(&[_][]const u8{ "journalctl", "--no-pager", "--output=short-iso" }, allocator);
+    child.stdout_behavior = .Pipe;
+    child.stderr_behavior = .Pipe;
+    try child.spawn();
+
+    var stdout_buffer: [1024]u8 = undefined;
+    var rdr_wrapper = child.stdout.?.reader(&stdout_buffer);
+    const rdr: *std.Io.Reader = &rdr_wrapper.interface;
+    while ((rdr.peekByte() catch 0) != 0) {
+        const line = rdr.takeDelimiterExclusive('\n') catch |err| {
+            switch (err) {
+                error.StreamTooLong => {
+                    _ = try rdr.discardDelimiterInclusive('\n');
+                    continue;
+                },
+                else => {
+                    std.process.exit(1);
+                },
+            }
+        };
+        // Toss the newline
+        rdr.toss(1);
+        std.debug.print("{s}\n", .{line});
+    }
+
+    _ = try child.wait();
 
     rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
     defer rl.closeWindow();
